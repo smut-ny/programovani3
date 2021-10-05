9c01b8cb6e4a3d6e9906e540b3ce2e6a8ccd6106
Vojtěch Smutný <vutristo@gmail.com>
2021-10-05 22:55:20 +0200

    my-flatten function

--- 1.py
+++ 1.py
@@ -4,7 +4,9 @@ def over_zavorky(string, stack_number):
     seznam_zavorek = list(string)
     stack = stack_number
     
-    if stack >= 0:
+    if stack < 0:
+        return False
+    else:
         if len(seznam_zavorek) != 0:
             if seznam_zavorek.pop() == ")":
                 return over_zavorky(seznam_zavorek, stack + 1)
@@ -14,15 +16,26 @@ def over_zavorky(string, stack_number):
             return True
         else:
             return False
-    else:
-        return False
+
+# print(
+# over_zavorky(")))))))((((((())))", 0)
+# )
 
 
+def my_flatten(lists):
+    flatten_list = []
+
+    for i in lists:
+        if type(i) is not list:
+            flatten_list.append(i)
+        else:
+            flatten_list.extend(my_flatten(i))
+
+    return flatten_list
 
 
+# seznamy = [100, 200, 300, [1, 2, 3, [10, 20, 30]]]
+# print(
+#     my_flatten(seznamy)
+# )
 
-
-
-print(
-over_zavorky(")))))))(((((((", 0)
-)
